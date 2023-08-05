fd0f810315fbd8152c98a0ca46e2afbf688b3252
monosans <hsyqixco@protonmail.com>
2023-08-05 13:20:11 +0300

    Minify package.json before publishing

---
+++ build/minify_package_json.mjs
@@ -0,0 +1,5 @@
+import fs from "fs";
+
+const json = fs.readFileSync("package.json", "utf8");
+const minifiedJson = JSON.stringify(JSON.parse(json));
+fs.writeFileSync("package.json", minifiedJson);
